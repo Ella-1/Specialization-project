aa0d57c15ad6c27bc501838d87895c6ce323ced8
Ella-1 <102160639+Ella-1@users.noreply.github.com>
2023-04-20 14:42:20 +0100

    react files

---
+++ client/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <div className='p-4 py-8 flex flex-col min-h-screen'>
+      <Header />
+      {/* outlet allow us maintain the same design in other files */}
+      <Outlet />
+    </div>
+  )
+}
